5e60b8d038ccc9e44b0d5ae3262f4acede96bed2
RafaelM <rafaelm.science@gmail.com>
2020-05-24 20:52:06 -0400

    init commit

--- app/src/main/java/com/rafaelm/poc_pacientes/MainActivity.kt
+++ app/src/main/java/com/rafaelm/poc_pacientes/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.rafaelm.poc_pacientes.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity(), MainNavigator {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding: MainActivity = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
         val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
